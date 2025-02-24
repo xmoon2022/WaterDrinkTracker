7c0268e9df52806076a4a2719f439ee905626f2f
xmoon2022 <2022270903004@std.uestc.edu.cn>
2025-02-24 15:30:26 +0800

    添加应用截图
    添加应用截图

--- app/src/main/java/io/github/xmoon2022/water/navigation/MainNavigation.kt
+++ app/src/main/java/io/github/xmoon2022/water/navigation/MainNavigation.kt
@@ -1,23 +1,10 @@
 package io.github.xmoon2022.water.navigation
 
-import androidx.compose.animation.AnimatedContentTransitionScope
-import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.foundation.gestures.detectHorizontalDragGestures
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableFloatStateOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
-import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
